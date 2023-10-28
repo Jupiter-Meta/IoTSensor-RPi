4b44ee969148d8c7175bc05367f80150d772974c
saishibunb <144234378+saishibunb@users.noreply.github.com>
2023-10-28 16:59:07 +0530

    Create mqttPublishDHT11.py

---
+++ testCodes/mqttPublishDHT11.py
@@ -0,0 +1,52 @@
+import Adafruit_DHT
+import paho.mqtt.client as mqtt
+import json
+import time
+
+# Define the DHT sensor type (DHT11)
+DHT_SENSOR = Adafruit_DHT.DHT11
+
+# Define the GPIO pin where the DHT sensor is connected
+DHT_PIN = 4  # Replace with the actual GPIO pin number
+
+# Define MQTT broker information
+MQTT_BROKER_HOST = "65.2.135.170"
+MQTT_BROKER_PORT = 1883
+MQTT_TOPIC = "JM/DHT"
+
+# Initialize MQTT client
+client = mqtt.Client()
+
+try:
+    # Attempt to read data from the DHT sensor
+    humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
+    
+    # Get the current epoch timestamp
+    timestamp = int(time.time())
+    
+    # Check if the data was successfully read
+    if humidity is not None and temperature is not None:
+        data = {
+            "temperature": temperature,
+            "humidity": humidity,
+            "timestamp": timestamp
+        }
+        
+        # Convert the data to JSON
+        json_data = json.dumps(data)
+        
+        print("Data to be published:")
+        print(json_data)
+        
+        # Publish the JSON data to the MQTT topic
+        client.connect(MQTT_BROKER_HOST, MQTT_BROKER_PORT, 60)
+        client.publish(MQTT_TOPIC, json_data)
+        client.disconnect()
+    else:
+        print("Failed to retrieve data from the DHT sensor")
+
+except KeyboardInterrupt:
+    # Exit the program if Ctrl+C is pressed
+    print("Program terminated by user")
+except Exception as e:
+    print(f"Error: {str(e)}")

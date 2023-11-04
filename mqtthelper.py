6e54437fb28cbcead85d88b5629407acf622dce4
saishibunb <144234378+saishibunb@users.noreply.github.com>
2023-11-04 12:04:34 +0530

    Update mqtthelper.py

--- mqtthelper.py
+++ mqtthelper.py
@@ -7,7 +7,7 @@ MQTT_BROKER_PORT = 1883
 
 client = mqtt.Client()
 
-def publish(MQTT_TOPIC, data)
+def publish(MQTT_TOPIC, data):
   
   client.connect(MQTT_BROKER_HOST, MQTT_BROKER_PORT, 60)
   client.publish(MQTT_TOPIC, json.dumps(data))

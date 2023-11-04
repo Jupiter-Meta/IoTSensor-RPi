b04569899e05bdebe3c001276c8b817ec58af363
Sai Shibu N B <saishibu@jupitermeta.io>
2023-11-04 11:58:24 +0530

    removed test codes

--- sdstest.py
+++
@@ -1,8 +0,0 @@
-from sds011lib import sensor_wake, sensor_sleep, sensor_read, getData
-
-sdssensorinit=getData()
-print(sdssensorinit)
-sdssensor=sensor_wake()
-print(sdssensor)
-sdsvalue=sensor_read
-print(sdsvalue)

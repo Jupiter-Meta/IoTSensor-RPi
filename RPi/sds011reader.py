cb429b8a15405a809cdd58ee3223e6ffe4973353
saishibunb <144234378+saishibunb@users.noreply.github.com>
2023-11-06 09:25:35 +0530

    Update sds011reader.py

--- RPi/sds011reader.py
+++ RPi/sds011reader.py
@@ -12,6 +12,8 @@ class SDS011Reader:
         self.serial = serial.Serial(port=inport,baudrate=9600)
 
     def readValue( self ):
+        sensor_wake()
+        time.sleep(15)
         step = 0
         while True: 
             while self.serial.inWaiting()!=0:
@@ -32,80 +34,21 @@ class SDS011Reader:
                     step =0
                     pm25 = float(values[0]+values[1]*256)/10 #divided by 10 to get correct values
                     pm10 = float(values[2]+values[3]*256)/10
+                    
                     return [pm25,pm10]
 
                 elif step>=2:
                     values[step-2]=v
                     step= step+1
-
-
-
-    def read( self, duration ):
-        start = os.times()[4]
-
-        count = 0
-        species = [[],[]]
-        speciesType = ["pm2.5-mg","pm10-mg"]
-
-        while os.times()[4]<start+duration:
-            try:
-                values = self.readValue()
-                species[0].append(values[0])
-                species[1].append(values[1])
-                count += 1
-                dt = os.times()[4]-start
-                print("[{:4.1f}] Samples:{:2d} PM2.5:{:4d} PM10:{:4d} StdDev(PM2.5):{:3.1f}".format(
-                    dt,count,values[0],values[1],np.std(species[0])
-                    ))
-                time.sleep(1)
-            except KeyboardInterrupt:
-                print("Bye")
-                sys.exit()
-            except:
-                e = sys.exc_info()[0]
-                print("Can not read the sensor data: "+str(e))
-
-        values = []
-        for i in range(len(species)):
-            values.append( dict( 
-                stddev = np.std(species[i]), 
-                median = np.median(species[i]),
-                min    = np.min(species[i]),
-                max    = np.max(species[i]),
-                avg    = np.average(species[i]),
-                type   = speciesType[i],
-                time   = datetime.now().isoformat(),
-                sensor = "SDS",
-                scale  = 1
-                ))
-
-        return values
-    
+        time.sleep(5)
+        sensor_sleep()
+        (time.sleep(3)
+        close()
     def close(self):
         self.serial.close()
 
     def sensor_wake(self):
-        bytes = ['\xaa', #head
-        '\xb4', #command 1
-        '\x06', #data byte 1
-        '\x01', #data byte 2 (set mode)
-        '\x01', #data byte 3 (sleep)
-        '\x00', #data byte 4
-        '\x00', #data byte 5
-        '\x00', #data byte 6
-        '\x00', #data byte 7
-        '\x00', #data byte 8
-        '\x00', #data byte 9
-        '\x00', #data byte 10
-        '\x00', #data byte 11
-        '\x00', #data byte 12
-        '\x00', #data byte 13
-        '\xff', #data byte 14 (device id byte 1)
-        '\xff', #data byte 15 (device id byte 2)
-        '\x05', #checksum
-        '\xab'] #tail
-        for b in bytes:
-            self.serial.write(b.encode())
+        self.serial.write('\x01')
     
     def sensor_sleep(self):
         bytes = ['\xaa', #head
@@ -128,4 +71,4 @@ class SDS011Reader:
         '\x05', #checksum+","+
         '\xab'] #tail
         for b in bytes:
-            self.serial.write(b.encode())
+            self.serial.write(b)

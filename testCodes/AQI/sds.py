705a91cd64014b9c52f181e5de66d187a935dd6b
saishibunb <144234378+saishibunb@users.noreply.github.com>
2023-11-06 09:48:54 +0530

    Create sds.py

---
+++ testCodes/AQI/sds.py
@@ -0,0 +1,27 @@
+from sds011 import SDS011
+import time
+
+# Replace 'COMx' with the actual COM port number (e.g., 'COM3')
+port = "COM3".encode('utf-8')  # Encode the port name as bytes
+
+def read_sds011_sensor(port):
+    try:
+        sds = SDS011(port=port)
+        sds.set_work_period(work_time=0)  # Set continuous mode
+
+        while True:
+            pm25, pm10 = sds.query()
+            print(f'PM2.5: {pm25} µg/m³, PM10: {pm10} µg/m³')
+
+            # Put the sensor to sleep and turn off the fan
+            sds.set_sleep(sleep=True)
+
+            time.sleep(2)  # Read data every 2 seconds
+
+    except KeyboardInterrupt:
+        # Set to sleep mode and turn off the fan before exiting
+        sds.set_sleep(sleep=True)
+        print('Exiting.')
+
+if __name__ == "__main__":
+    read_sds011_sensor(port)

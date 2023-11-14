912219bc55b868a4f3b10077aad2a832405e21a3
saishibunb <144234378+saishibunb@users.noreply.github.com>
2023-11-14 11:43:01 +0530

    Update mqtthelper.py

--- RPi/mqtthelper.py
+++ RPi/mqtthelper.py
@@ -1,13 +1,14 @@
 import paho.mqtt.client as mqtt
 import json
 
-MQTT_BROKER_HOST = "65.2.135.170"
-MQTT_BROKER_PORT = 1883
+
 # MQTT_TOPIC = "JM/ALLSENSOR"
 
 client = mqtt.Client()
 
-def publish(MQTT_TOPIC, data):
+def publish(broker, MQTT_TOPIC, data):
+  MQTT_BROKER_HOST = broker
+  MQTT_BROKER_PORT = 1883
   
   client.connect(MQTT_BROKER_HOST, MQTT_BROKER_PORT, 60)
   client.publish(MQTT_TOPIC, json.dumps(data))
